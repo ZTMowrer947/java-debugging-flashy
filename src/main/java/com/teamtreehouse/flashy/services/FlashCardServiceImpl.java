e75236e78659f16e15e390fe9135abdc2fbb4771
Zackery Mowrer <Zackmowrer@gmail.com>
2020-04-10 12:06:07 -0400

    Clean up card retrieval methods

--- src/main/java/com/teamtreehouse/flashy/services/FlashCardServiceImpl.java
+++ src/main/java/com/teamtreehouse/flashy/services/FlashCardServiceImpl.java
@@ -2,15 +2,10 @@ package com.teamtreehouse.flashy.services;
 
 import com.teamtreehouse.flashy.domain.FlashCard;
 import com.teamtreehouse.flashy.repositories.FlashCardRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 import static java.util.stream.Collectors.toList;
 
@@ -52,21 +47,21 @@ public class FlashCardServiceImpl implements FlashCardService {
   @Override
   public FlashCard getNextFlashCardBasedOnViews(Map<Long, Long> idToViewCounts) {
     FlashCard card = getNextUnseenFlashCard(idToViewCounts.keySet());
-    if (card != null) {
-      return card;
+    if (card == null) {
+      card = getLeastViewedFlashCard(idToViewCounts);
     }
+    return card;
+  }
+
+  public FlashCard getLeastViewedFlashCard(Map<Long, Long> idToViewCounts) {
     Long leastViewedId = null;
-    for (Map.Entry<Long, Long> entry : idToViewCounts.entrySet()) {
-      if (leastViewedId == null) {
-        leastViewedId = entry.getKey();
-        continue;
-      }
-      Long lowestScore = idToViewCounts.get(leastViewedId);
-      if (entry.getValue() < lowestScore) {
-        leastViewedId = entry.getKey();
-      }
-    }
-    return flashCardRepository.findOne(leastViewedId);
+    List<Map.Entry<Long, Long>> entries = new ArrayList<>(idToViewCounts.entrySet());
+    Collections.shuffle(entries);
+
+    return entries.stream()
+            .min(Map.Entry.comparingByValue())
+            .map(entry -> flashCardRepository.findOne(entry.getKey()))
+            .orElseThrow(IllegalArgumentException::new);
   }
 
   @Override
